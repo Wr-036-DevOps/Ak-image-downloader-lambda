3b4d8c18e65a6c2e72fa3d22ca5a04880220bf64
Ak <akachukwumba@gmail.com>
2022-11-16 12:31:11 +0100

    Updated to create an archive file before apply

--- main.tf
+++ main.tf
@@ -20,7 +20,11 @@ provider "aws" {
     # secret_key = "my-secret-key"
 }
 
-#Creating Lambda Role
+
+
+
+# Creating IAM Role for lambda function
+
 resource "aws_iam_role" "Training-lambda_image_downloader_role" {
     name = "Training-lambda_image_downloader_role"
     permissions_boundary  = "arn:aws:iam::536460581283:policy/boundaries/CustomPowerUserBound"
@@ -38,9 +42,7 @@ resource "aws_iam_role" "Training-lambda_image_downloader_role" {
         ]
     })
 }
-
-
-#Attaching S3 access policy
+# Attaching S3 access policy to the lambda role
 resource "aws_iam_role_policy_attachment" "S3-access-policy" {
     role       = "${aws_iam_role.Training-lambda_image_downloader_role.name}"
     policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
@@ -48,21 +50,21 @@ resource "aws_iam_role_policy_attachment" "S3-access-policy" {
 
 
 
-#Creating Public S3 Bucket
-resource "aws_s3_bucket" "b" {
+
+# Creating Public S3 Bucket for downladed images
+
+resource "aws_s3_bucket" "image_bucket" {
     bucket_prefix = "lambda-images"
 }
+# Making the S3 Bucket public
 resource "aws_s3_bucket_public_access_block" "example" {
-    bucket = "${aws_s3_bucket.b.id}"
+    bucket = "${aws_s3_bucket.image_bucket.id}"
     block_public_policy = false
     
 }
-
-#Attaching S3 Policy
-
+# Attaching S3 Bucket Policy to allow public access to the objects
 resource "aws_s3_bucket_policy" "Public_access" {
-    bucket = aws_s3_bucket.b.id
-    #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+    bucket = aws_s3_bucket.image_bucket.id
     policy = <<EOF
 {
 	"Version": "2012-10-17",
@@ -71,7 +73,7 @@ resource "aws_s3_bucket_policy" "Public_access" {
 			"Effect": "Allow",
 			"Principal": "*",
 			"Action": "s3:GetObject",
-			"Resource": "${aws_s3_bucket.b.arn}/*"
+			"Resource": "${aws_s3_bucket.image_bucket.arn}/*"
         
 		}
 	]
@@ -80,14 +82,22 @@ EOF
 }
 
 
+# Creating Lambda Function archive/zip file 
 
+data "archive_file" "init" {
+    type        = "zip"
+    source_dir  = "${path.module}/python"
+    output_path = "${path.module}/deployment_package.zip"
 
-#Creating Lambda Fxn 
+}
 
-resource "aws_lambda_function" "test_lambda" {
-    filename      = "my-deployment-package.zip"
+# Creating Lambda function
+
+resource "aws_lambda_function" "image_downloader_lambda" {
+    filename      = "deployment_package.zip"
     function_name = "image-downloader"
     role          = aws_iam_role.Training-lambda_image_downloader_role.arn
     runtime       = "python3.8"
-    handler       = "lambda_handler"
+    handler       = "lambda_function.lambda_handler"
+    timeout       = 15
 }
